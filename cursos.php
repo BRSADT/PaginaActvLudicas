5d967fa18c11f82c53c3f07e8340d641233dee22
MrSalad <carlos_rc97@outlook.es>
2016-09-13 22:47:03 -0500

    asfasdfsadfasdfsaf

--- cursos.php
+++ cursos.php
@@ -23,7 +23,7 @@
 		<ul>
 	
 	
-			<li><a  href="#" class="hvr-underline-from-right" >Inicio</a></li>
+			<li><a  href="ActvLudicas.php" class="hvr-underline-from-right" >Inicio</a></li>
 			<li><a href="#" class="hvr-underline-from-right">Tutoriales</a></li>
 			<li><a href="#" class="hvr-underline-from-right">Cursos</a></li>
 			<li><a href="#" class="hvr-underline-from-right">Preguntame algo</a></li>
