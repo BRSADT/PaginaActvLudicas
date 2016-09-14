b075959524896bd0931c048640060a3053dd1083
BRSADT <brendasamant@hotmail.com>
2016-09-13 22:41:39 -0500

    ldsakadlk

--- ActvLudicas.php
+++ ActvLudicas.php
@@ -12,7 +12,7 @@
 <header>
 
 <span class="classy"><input type="email" placeholder="Correo Electronico"></span>
-<span class="classy"><input type="email" placeholder="Contraseña"></span>
+<span class="classy"><<INPUT type="text" STYLE="color: #FFFFFF; font-family: Verdana; font-weight: bold; font-size: 12px; background-color: #72A4D2;" size="10" maxlength="30">></span>
 <p> </p>
 
 
