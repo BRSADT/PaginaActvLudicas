7d769d8fc627f0f33e370e97a15529164ad9a715
MrSalad <carlos_rc97@outlook.es>
2016-09-13 23:23:05 -0500

    sha esta

---
+++ ventas.php
@@ -0,0 +1,66 @@
+<html>
+<head>
+<head>
+  <title>Un proyecto sin Nombre</title>
+  <link rel="stylesheet" href="Estilos.css">
+</head>
+
+<body>
+
+
+
+<header>
+
+<span class="classy"><input type="email" placeholder="Correo Electronico"></span>
+<span class="classy"><<INPUT type="text" STYLE="color: #FFFFFF; font-family: Verdana; font-weight: bold; font-size: 12px; background-color: #72A4D2;" size="10" maxlength="30">></span>
+<p> </p>
+
+
+<div class="grad-nex">
+</div>
+	<nav>
+		<ul>
+	
+	
+			<li><a  href="ActvLudicas.php" class="hvr-underline-from-right" >Inicio</a></li>
+			<li><a href="#" class="hvr-underline-from-right">Tutoriales</a></li>
+			<li><a href="cursos.php" class="hvr-underline-from-right">Cursos</a></li>
+			<li><a href="#" class="hvr-underline-from-right">Preguntame algo</a></li>
+			<li><a href="#" class="hvr-underline-from-right">Contacto</a></li>
+			
+		</ul>
+	</nav>
+</header>
+
+<section class="contenido wrapper">
+    <div class="contenido wrapper venta">
+            <div class="contenido wrapper venta ventax">
+        <h1>Producto 1</h1>
+<p>$100</p>
+<IMG SRC="leapmotion.jpg">
+<hr>
+        <h1>Producto 2</h1>
+<p>$200</p>
+<IMG SRC="leap.png">
+<hr>
+        <h1>Producto 3</h1>
+<p>$300</p>
+<IMG SRC="marcianito.gif">
+<hr>
+    </div>
+    </div>
+</section>
+    <section class="contenido wrapper">
+    <div class="contenido wrapper contacto">
+            <div class="contenido wrapper inicio contacto">
+        <h1>Hola</h1>
+<p>No nos llame por favor :3</p>
+    </div>
+    </div>
+</section>
+
+</body>
+
+  
+  
+</html> 
